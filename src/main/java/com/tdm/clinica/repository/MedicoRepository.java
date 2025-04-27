3af901bb252b0b03ceae390b1c5c76b0c7256d88
GabrielLomba12 <lombagabriel324@gmail.com>
2025-04-27 20:28:51 -0300

    fix: bringing a list of doctors

--- src/main/java/com/tdm/clinica/repository/MedicoRepository.java
+++ src/main/java/com/tdm/clinica/repository/MedicoRepository.java
@@ -4,10 +4,11 @@ import com.tdm.clinica.model.CredencialModel;
 import com.tdm.clinica.model.MedicoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MedicoRepository extends JpaRepository<MedicoModel, Long> {
     Optional<MedicoModel> findByCredencialModel(CredencialModel credencial);
 
-    MedicoModel findByEspecialidade(String especialidade);
+    List<MedicoModel> findByEspecialidade(String especialidade);
 }
